c427945ce6333545a3787cf3d40d9e943cd33810
louie.mansour@loblaw.ca <louie.mansour@loblaw.ca>
2020-12-28 09:29:22 -0500

    Title fix and added twitter link

--- src/links/Links.js
+++ src/links/Links.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import "./links.css"
 
@@ -10,6 +10,7 @@ function Links() {
             <a href={"https://www.linkedin.com/in/louie-mansour-b2630a43/?originalSubdomain=ca"} target={"_blank"} rel={"noreferrer"}><FontAwesomeIcon id={"linkedin-icon"} className={"link-icon"} icon={faLinkedin} /></a>
             <a href={"https://github.com/louie-mm"} target={"_blank"} rel={"noreferrer"}><FontAwesomeIcon id={"github-icon"} className={"link-icon"} icon={faGithub} /></a>
             <a href={"mailto:career@louiem.dev"} target={"_blank"} rel={"noreferrer"}><FontAwesomeIcon id={"email-icon"} className={"link-icon"} icon={faEnvelope} /></a>
+            <a href={"https://twitter.com/DevLouiem"} target={"_blank"} rel={"noreferrer"}><FontAwesomeIcon id={"twitter-icon"} className={"link-icon"} icon={faTwitter} /></a>
         </div>
     );
 }
